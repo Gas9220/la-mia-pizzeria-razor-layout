64d6da7f4406da8c5a0d8b1d956b94d7fca2fdbe
Gaspare <Gas9220@users.noreply.github.com>
2023-09-28 16:03:37 +0200

    PizzaStore

---
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -0,0 +1,19 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhotoUrl { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+
+        public Pizza(int id, string name, string photoUrl, string description, float price) { 
+            Id = id;
+            Name = name;
+            PhotoUrl = photoUrl;
+            Description = description;
+            Price = price;
+        }
+    }
+}
---
+++ la-mia-pizzeria-static/Models/PizzaStore.cs
@@ -0,0 +1,16 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public static class PizzaStore
+    {
+        public static List<Pizza> GetAllPizzas()
+        {
+            List<Pizza> pizzaList = new List<Pizza>() {
+             new Pizza(1, "Margherita", "/img/margherita.png", "Pomodoro, mozzarella", 6),
+            new Pizza(2, "Marinara", "/img/marinara.png", "Pomodoro, origano, aglio", 5),
+            new Pizza(3, "Diavola", "/img/diavola.png", "Pomodoro, mozzarella, salame piccante", 8)
+            };
+
+            return pizzaList;
+        }
+    }
+}
